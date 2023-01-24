3e35a21ebbb3a4699d3ecc7d84e61d1c17c8b524
Uliana Cheklina <uche0790@gmail.com>
2023-01-25 00:53:51 +0100

    Create header and nav, start the main section

--- src/App.js
+++ src/App.js
@@ -1,24 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import './fonts/Ondo.ttf';
+import './fonts/Arial.ttf';
+import './fonts/Circular.ttf';
+import './fonts/Circular-book.ttf';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+
+const bodyStyle = css`
+  background-color: black;
+`;
+const headerBar = css`
+  z-index: 1000;
+  background-color: black;
+  display: flex;
+`;
+const navBar = css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+  margin: 0 250px;
+  height: 80px;
+  font-family: 'Inter', sans-serif;
+  font-weight: 550;
+  letter-spacing: 0.6px;
+  font-size: 16px;
+  color: #fff;
+  position: relative;
+  float: right;
+
+  a,
+  a:link,
+  a:visited {
+    color: #ffffff;
+    text-decoration: none;
+  }
+  ul {
+    display: flex;
+    gap: 150px;
+    position: relative;
+    float: right;
+    padding: 0 0 0 330px;
+  }
+  .logo,
+  li {
+    list-style-type: none;
+  }
+  img {
+    font-size: 20px;
+    letter-spacing: 0.7px;
+  }
+  li:hover {
+    text-decoration: underline;
+    transition-delay: text-decoration 3s;
+    padding: 3px 0 0;
+    // box-shadow: 1px 2px 12px #e63963;
+  }
+`;
+const h1Wrapper = css`
+  background-color: black;
+  color: white;
+  h1 {
+    font-family: 'Arial';
+    font-weight: 1000;
+    letter-spacing: -2px;
+    font-size: 50px;
+    span {
+      color: #e63963;
+    }
+  }
+  p {
+    font-family: 'Inter', sans-serif;
+    font-weight: 600;
+    font-size: 25px;
+    line-height: 1.6;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <body css={bodyStyle}>
+      <header css={headerBar}>
+        <nav css={navBar}>
+          <img src="/images/Logo.svg" alt="HotDrop" width="100" />
+          <ul>
+            <li>
+              <a href="/#">Get in touch</a>
+            </li>
+            <li>
+              <a href="/#">About</a>
+            </li>
+            <li>
+              <a href="/#">Join our team</a>
+            </li>
+          </ul>
+        </nav>
       </header>
-    </div>
+      <main>
+        <div css={h1Wrapper}>
+          <h1>
+            Discover music.
+            <br />
+            <span>Support artists.</span>
+          </h1>
+
+          <p>
+            Stuck listening to the same mainstream songs and artists on-repeat?
+            <br />
+            Keep your music taste fresh with millions of new songs you've never
+            heard before.
+            <br />
+            Join the movement, become a superfan.
+          </p>
+          <img src="/images/index-image.png" />
+          <button id="white">DISCOVER MUSIC</button>
+          <button id="red">PROMOTE MUSIC</button>
+        </div>
+      </main>
+      <footer>Some info</footer>
+    </body>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
+import './fonts/Ondo.ttf';
+import './fonts/Arial.ttf';
+import './fonts/Circular.ttf';
+import './fonts/Circular-book.ttf';
+import { css, Global } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Global
+      styles={css`
+        @font-face {
+          font-family: 'Arial';
+          src: local('Arial'), url() ('./fonts/Arial.ttf') format('truetype');
+          font-family: 'Inter', sans-serif;
+        }
+        body {
+          margin: 0;
+        }
+        *,
+        ::after,
+        ::before {
+          box-sizing: border-box;
+          // text-decoration: none;
+          // ondo 700,circular book 400, circular 400 500 700 900,arial 400
+        }
+      `}
+    />
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
