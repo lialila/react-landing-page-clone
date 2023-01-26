d9dfdd93d2ca602fbb24d3ec60d369ad7b7dc048
Uliana Cheklina <uche0790@gmail.com>
2023-01-26 01:59:18 +0100

    Create section with review cards

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import './fonts/Ondo.ttf';
 import './fonts/Arial.ttf';
-import './fonts/Circular.ttf';
-import './fonts/Circular-book.ttf';
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 
 const bodyStyle = css`
-  box-sizing: border-box;
+  // box-sizing: border-box;
   background-image: url('https://uploads-ssl.webflow.com/630e348760dd00665a03452e/63491d729c6a15033540e97e_Group%201113%20(2).png');
   background-color: black;
   background-repeat: no-repeat;
   background-position: 75%;
   background-size: 560px;
   background-position: 730px 130px;
-  font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif;
   color: white;
 `;
 
 const headerBar = css`
-  background-color: black;
   display: flex;
 `;
 
 const navBar = css`
-  //position: fixed;
+  background-color: black;
+  position: fixed;
   z-index: 10;
   display: flex;
   align-items: center;
@@ -36,7 +32,7 @@ const navBar = css`
   letter-spacing: 0.6px;
   font-size: 16px;
   color: #fff;
-  position: relative;
+  box-shadow: 0px 8px 15px black;
   float: right;
 
   a,
@@ -73,9 +69,9 @@ const navBar = css`
 `;
 
 const h1Wrapper = css`
-  margin-bottom: 100px;
+  margin-bottom: 210px;
   margin-left: 200px;
-  margin-top: 100px;
+  margin-top: 180px;
   display: flex;
   justify-content: space-between;
   flex-direction: column;
@@ -103,7 +99,6 @@ const h1Wrapper = css`
     font-family: 'Inter', sans-serif;
     font-weight: 500;
     font-size: 22px;
-    line-height: 1.3;
     margin: 12px 0 50px;
   }
 
@@ -125,23 +120,170 @@ const h1Wrapper = css`
   }
 `;
 
-const wrapperh2 = css`
-  display: inline-block;
-  //  justify-content: space-evenly;
-  max-width: 100%;
-
+const h2Wrapper = css`
   h2 {
-    align-items: center;
-    font-size: 14px;
+    color: white;
+    margin: 80px 0 30px;
     font-family: 'Arial', 'Helvetica', sans-serif;
-    font-weight: 500;
+    font-weight: 550;
+    letter-spacing: 0.2px;
+    font-size: 38px;
+    line-height: 50px;
+    display: flex;
+    justify-content: center;
+  }
+`;
+const featured = css`
+  margin-top: -20px;
+  margin-bottom: -25px;
+`;
+
+const portfolioWrapper = css`
+  display: flex;
+  justify-content: center;
+  gap: 130px;
+
+  img {
+    border-radius: 10px;
   }
 `;
 
-const wrapperGrid = css`
-  display: grid;
-  grid-auto-columns: 30px;
-  grid-gap: 100px;
+const partLeft = css`
+  display: flex;
+  justify-content: space-between;
+  //flex-direction: flex-column;
+  padding: 170px 0;
+  max-width: 1200px;
+  margin: 0 250px 20px 250px;
+  img {
+    z-index: 3;
+    width: 22rem;
+    object-position: 0px 0px;
+    padding-left: -100px;
+  }
+
+  h3 {
+    font-family: 'Arial', 'Helvetica', sans-serif;
+    font-weight: 800;
+    letter-spacing: 0.6px;
+    font-size: 40px;
+    line-height: 50px;
+    margin-top: 230px;
+  }
+  p {
+    letter-spacing: 0.8px;
+    line-height: 30px;
+    font-family: 'Inter', sans-serif;
+    font-weight: 500;
+    font-size: 22px;
+  }
+`;
+
+const partRight = css`
+  display: flex;
+  margin-bottom: 20px;
+  justify-content: space-between;
+  flex-direction: flex-end;
+  padding: 170px 0;
+  max-width: 1200px;
+  margin: 20px 250px;
+  text-align: right;
+  img {
+    width: 22rem;
+  }
+
+  h3 {
+    font-family: 'Arial', 'Helvetica', sans-serif;
+    font-weight: 800;
+    letter-spacing: 0.6px;
+    font-size: 40px;
+    line-height: 50px;
+    margin-top: 230px;
+  }
+  p {
+    letter-spacing: 0.8px;
+    line-height: 30px;
+    font-family: 'Inter', sans-serif;
+    font-weight: 500;
+    font-size: 22px;
+  }
+`;
+
+const partRight1 = css`
+  display: flex;
+  margin-bottom: 20px;
+  justify-content: space-between;
+  flex-direction: flex-end;
+  padding: 170px 0;
+  max-width: 1200px;
+  margin: 20px 250px;
+  text-align: right;
+
+  h3 {
+    font-family: 'Arial', 'Helvetica', sans-serif;
+    font-weight: 800;
+    letter-spacing: -2.6px;
+    font-size: 40px;
+    line-height: 50px;
+    margin-top: 180px;
+  }
+  p {
+    letter-spacing: 0.8px;
+    line-height: 30px;
+    font-family: 'Inter', sans-serif;
+    font-weight: 500;
+    font-size: 22px;
+  }
+
+  img {
+    width: 600px;
+  }
+
+  a {
+    color: #e63963;
+  }
+`;
+const test = css`
+  display: flex;
+  flex-direction: row;
+  // align-items: center;
+`;
+
+const boxes = css`
+  border-radius: 45px;
+  color: black;
+  background-color: white;
+  width: 360px;
+  height: 90px;
+  margin: 10px 0;
+  gap: 30px;
+
+  img {
+    border-radius: 50%;
+    width: 62px;
+    position: static;
+    margin: 13px 15px;
+    float: left;
+  }
+
+  p {
+    font-family: 'Arial', sans-serif;
+    font-size: 14px;
+    color: #333333;
+    text-align: left;
+    padding-top: 21px;
+  }
+`;
+const testiH3 = css`
+  display: flex;
+  margin: auto;
+  justify-content: center;
+  font-family: 'Arial', sans-serif;
+  font-weight: 550;
+  letter-spacing: 0.2px;
+  font-size: 30px;
+  line-height: 50px;
+  margin-bottom: 50px;
 `;
 
 function App() {
@@ -168,7 +310,7 @@ function App() {
         </nav>
       </header>
       <main>
-        <div css={h1Wrapper}>
+        <section css={h1Wrapper}>
           <h1>
             Discover music.
             <br />
@@ -195,45 +337,173 @@ function App() {
               <button className="red">PROMOTE MUSIC</button>
             </a>
           </div>
-        </div>
-        <div style={wrapperh2}>
+        </section>
+        <section css={h2Wrapper}>
           <h2>Featured in</h2>
-          <div style={wrapperGrid}>
+          <div css={featured}>
             <img
               src="https://uploads-ssl.webflow.com/630e348760dd00665a03452e/63980dce3e8200701d125e27_F1.png"
               alt="WSJ"
-              width="20%"
+              width="25%"
             />
             <img
               src="https://uploads-ssl.webflow.com/630e348760dd00665a03452e/63980dcde7aba3a29be95809_F2.png"
               alt="musically"
-              width="20%"
+              width="25%"
             />
 
             <img
               src="https://uploads-ssl.webflow.com/630e348760dd00665a03452e/63980dce67761f561f271dbb_F3.png"
               alt="Hyperbot"
-              width="20%"
+              width="25%"
             />
             <img
               src="https://uploads-ssl.webflow.com/630e348760dd00665a03452e/63980dce1925274830086792_F4.png"
               alt="BostInno"
-              width="20%"
+              width="25%"
             />
           </div>
-          <div>
-            <h2>A proud portfolio company of</h2>
-
+          <h2>A proud portfolio company of</h2>
+          <div css={portfolioWrapper}>
             <img
               src="https://uploads-ssl.webflow.com/630e348760dd00665a03452e/632149ed41e0510de6e577ad_Techstars_Blinking.png"
               alt="techstars"
+              width="20%"
             />
             <img
               src="https://uploads-ssl.webflow.com/630e348760dd00665a03452e/632149ed41e05152cde577b2_Screen%2520Shot%25202022-09-13%2520at%25206.34.19%2520PM-p-500.png"
               alt="ventures"
+              width="20%"
             />
           </div>
-        </div>
+        </section>
+        <section css={partLeft}>
+          <div>
+            <h3>
+              Connect with new <br />
+              artists & support <br />
+              their journey.
+            </h3>
+            <p>
+              90% of artists are invisible. We put artist stories in
+              <br /> the spotlight.
+            </p>
+          </div>
+          <div>
+            <img
+              src="https://uploads-ssl.webflow.com/630e348760dd00665a03452e/63481f87fae764870c1abdcb_Group%201110-p-500.png"
+              alt="depend on"
+            />
+          </div>
+        </section>
+        <section css={partRight}>
+          <div>
+            <img
+              src="https://uploads-ssl.webflow.com/630e348760dd00665a03452e/6310b8256fb391d0b5907328_Genres-2_iphone13midnight_portrait-p-500.png"
+              alt=""
+              genres
+            />
+          </div>
+          <div>
+            <h3>
+              You control the
+              <br /> discovery experience.
+            </h3>
+            <p>
+              Explore a feed of new music you haven't
+              <br /> heard before – guaranteed.
+            </p>
+          </div>
+        </section>
+        <section css={partLeft}>
+          <div>
+            <h3>
+              Be the reason artists
+              <br /> go viral.
+            </h3>
+            <p>
+              Share hidden-gems with your friends. <br />
+              Be early on the next star in music.
+            </p>
+          </div>
+          <div>
+            <img
+              src="https://uploads-ssl.webflow.com/630e348760dd00665a03452e/6347f1949d97359f010efecf_Group%201111-p-500.png"
+              alt="trends"
+            />
+          </div>
+        </section>
+        <section css={partRight1}>
+          <div>
+            <img
+              src="https://uploads-ssl.webflow.com/630e348760dd00665a03452e/637a9f98ecc8b47c728bbeba_Group%201299-2_macbookgrey_front-p-800.png"
+              alt="file upload"
+              width="600px"
+            />
+          </div>
+          <div>
+            <h3>
+              Artists promote <br />
+              directly to Gen-Z.
+            </h3>
+            <p>
+              Upload your songs, reach new <br />
+              superfans, and build community.
+            </p>
+            <div>
+              <p>
+                <a href="/#">Sign up</a> for HotDrop Studio.
+              </p>
+            </div>
+          </div>
+        </section>
+        <h3 css={testiH3}>Testimonials</h3>
+        <section css={test}>
+          <div>
+            <div css={boxes}>
+              <img
+                src="https://uploads-ssl.webflow.com/630e348760dd00665a03452e/630e348760dd003f8903458d_8.jpg"
+                alt="Aaron"
+              />
+              <p>"HotDrop and Spotify is the ultimate combo" - Aaron</p>
+            </div>
+            <div css={boxes}>
+              <img
+                src="https://uploads-ssl.webflow.com/630e348760dd00665a03452e/630e348760dd0017ea0345a0_16.jpg"
+                alt="Jordam"
+              />
+              <p>Before HotDrop I never liked Rock music" - Jordan</p>
+            </div>
+            <div css={boxes}>
+              <img
+                src="https://uploads-ssl.webflow.com/630e348760dd00665a03452e/630e348760dd00863903459e_15.jpg"
+                alt="Samantha"
+              />
+              <p>"How is this not in the Top Charts?!" - Sammantha</p>
+            </div>
+            <div css={boxes}>
+              <img
+                src="https://uploads-ssl.webflow.com/630e348760dd00665a03452e/630e348760dd00bdac034589_5.jpg"
+                alt="001"
+              />{' '}
+              <p>"Easy to get lost and pass time like TikTok.” – Mike</p>
+            </div>
+            <div css={boxes}>
+              <img
+                src="https://uploads-ssl.webflow.com/630e348760dd00665a03452e/630e348760dd00304003459d_13.jpg"
+                alt="002"
+              />
+              <p>"Every college student needs to download HotDrop" - Matt</p>
+            </div>
+          </div>
+          <div css={boxes}>
+            <img
+              src="https://uploads-ssl.webflow.com/630e348760dd00665a03452e/630e348760dd00e10a03458b_3.jpg"
+              alt="003"
+            />
+            <p>"HotDrop and Spotify is the ultimate combo" - Aaron</p>
+          </div>
+        </section>
       </main>
       <footer>Some info</footer>
     </body>
